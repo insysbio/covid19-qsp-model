66c52a69a4c9343dd79cd285194288116cdc71c8
Evgeny Metelkin <metelkin@insysbio.com>
2022-01-10 13:04:54 +0300

    fix simbio export

--- diagnostics/simbio/start.sh
+++ diagnostics/simbio/start.sh
@@ -1,2 +1,2 @@
 # before run the nameless.m must be renamed to nameless_.m
-matlab.exe -sd "." -nodisplay -nosplash -nodesktop -wait -r "run('dist/covid19_integrated_simbio/nameless_.m');run('diagnostics/simbio/run.m');"
+matlab.exe -sd "." -nodisplay -nosplash -nodesktop -wait -r "run('dist/covid19_integrated_simbio/nameless.m');run('diagnostics/simbio/run.m');"
